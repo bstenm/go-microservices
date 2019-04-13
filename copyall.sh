53642a25cd6459765891601425996a26fc2a5ab4
Bertrand Steinmetz <bstenm@hotmail.com>
2019-04-13 10:46:25 -0500

    starting quote service

--- copyall.sh
+++ copyall.sh
@@ -5,11 +5,16 @@ export CGO_ENABLED=0
 cd accountservice;go get;go build -o accountservice-linux-amd64;echo built `pwd`;cd ..
 # build healthchecker
 cd healthchecker;go get;go build -o healthchecker-linux-amd64;echo built `pwd`;cd ..
+# build quote service
+cd quoteservice;go get;go build -o quoteservice-linux-amd64;echo built `pwd`;cd ..
 
 # copy healthchecker binary into the account service folder
 cp healthchecker/healthchecker-linux-amd64 accountservice/
 
 docker build -t bstenm/accountservice accountservice/
+docker build -t bstenm/quoteservice quoteservice/
 
 docker service rm accountservice
+docker service rm quoteservice
 docker service create --name=accountservice --replicas=1 --network=my_network -p=6767:6767 bstenm/accountservice
+docker service create --name=quoteservice --replicas=1 --network=my_network -p=6768:6768 bstenm/quoteservice
